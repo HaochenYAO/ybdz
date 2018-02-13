d23307a8a98d58b780c75b1bbfd073419575d056
yaohaochen <yaohaochen@fangdd.com>
2018-02-13 22:13:53 +0800

    [INT]init-2

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,10 +1,4 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
-}
-
-render(<App/>, document.getElementById('app'));
+ReactDOM.render(<div>aaa</div>, document.getElementById('root'));
---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+import Koa from 'koa';
+import views from 'koa-views';
+import Router from 'koa-router';
+import path from 'path';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(require('koa-static')(path.join(__dirname, '../client/build')));
+app.use(views(path.join(__dirname, '../client/views'), {
+  extension: 'html'
+}));
+
+router.get('/', async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+// response
+app.use(async(ctx) => {
+  await ctx.render('index.html');
+});
+
+app.listen(process.env.PORT || 3001);
