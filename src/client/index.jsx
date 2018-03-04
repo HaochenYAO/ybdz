292df6a4c614772d6a79a3e82510888a3bb4e586
yaohaochen <yaohaochen@fangdd.com>
2018-03-04 14:35:35 +0800

    [OPT]elimate log useless

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -13,7 +13,7 @@ const storeParam = [
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ];
-console.log(...storeParam);
+
 const store = configureStore(...storeParam);
 const rootElement = document.getElementById('root');
 
